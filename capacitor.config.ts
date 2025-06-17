94ca83fb48e3b7b1ab49ae3220df0ac3bfd0d865
Aluisio Lima <aaluisio309@gmail.com>
2025-06-17 08:38:35 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pokeap_angula',
+  webDir: 'www'
+};
+
+export default config;
